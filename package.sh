6bf2a72b38a850c0f7403c9be87b2bf5db7f8c4d
esteinmann <63729650+esteinmann@users.noreply.github.com>
2022-12-23 19:57:39 +0100

    Grep version number from manifest

--- package.sh
+++ package.sh
@@ -3,18 +3,22 @@
 # Create releases dir when it does not exist.
 mkdir -p releases
 
-# Validate $1.
-version_number_pattern="^[0-9]+\.[0-9]+$"
-if [[ ! $1 =~ $version_number_pattern ]]; then
-    echo "Value for first argument $1 is not a valid version number. Needs to match regex pattern $version_number_pattern"
-    exit 1;
+# Extract version from manifest.
+version=$(grep -oP '"version": "\K[0-9]+\.[0-9]' src/manifest.json)
+
+if [[ ! $version ]]; then
+    echo "Failed to extract version from src/manifest.json"
+    exit 1
 fi
 
 # Construct a file name with the given version number.
-zip_file="chatgpt_convdown$1.zip"
+zip_file="chatgpt_convdown$version.zip"
 
 # Move to source directory and zip it. 
 cd src && zip -r ../releases/$zip_file *
 
 # Add the LICENSE file.
 cd .. && zip -u releases/$zip_file LICENSE
+
+# Report result.
+echo "Created package: $zip_file"
