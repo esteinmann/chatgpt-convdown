e76b9bbd0c56317a083fcca9814b2411a8ad3c97
esteinmann <63729650+esteinmann@users.noreply.github.com>
2022-12-23 17:16:49 +0100

    Split background and content again + housekeeping
    Create a separate script for background and content (download-button.js) and refactor the background script. also added a message from ChatGPT to README.md and make the package.sh script put the packages in a releases folder.

--- package.sh
+++ package.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Validate $1
+# Create releases dir when it does not exist.
+mkdir -p releases
+
+# Validate $1.
 version_number_pattern="^[0-9]+\.[0-9]+$"
 if [[ ! $1 =~ $version_number_pattern ]]; then
     echo "Value for first argument $1 is not a valid version number. Needs to match regex pattern $version_number_pattern"
@@ -11,7 +14,7 @@ fi
 zip_file="chatgpt_convdown$1.zip"
 
 # Move to source directory and zip it. 
-cd src && zip -r ../$zip_file *
+cd src && zip -r ../releases/$zip_file *
 
 # Add the LICENSE file.
-cd .. && zip -u $zip_file LICENSE
+cd .. && zip -u releases/$zip_file LICENSE
---
+++ src/background.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2022 Erik Steinmann
+ * 
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+// Filter so the history update events are only triggered for chat.openai.com.
+const filter = {    
+    url: [{hostEquals: "chat.openai.com"}]
+};
+
+const listener = async (evt) => {
+    // Filter out any sub-frame related navigation event.
+    if (evt.frameId !== 0) {
+        return;
+    }
+    
+    // TODO Make logging configurable (to disable it in release mode for example) 
+    console.log("history updated: " + evt.url);
+    
+    // Execute content script to add download button.
+    await browser.scripting.executeScript({
+        target: {
+            tabId: evt.tabId,
+        },
+        files: ["download-button.js"]
+    });
+};
+
+// Listen for history changes on chat.openai.com.
+browser.webNavigation.onHistoryStateUpdated.addListener(listener, filter);
