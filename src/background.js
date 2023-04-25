3b27c6345b06b3eddf3c97f22eb164b5eadd7f0a
esteinmann <63729650+esteinmann@users.noreply.github.com>
2023-04-25 21:45:59 +0200

    Listen to web request to re-add button after chat is loaded

--- src/background.js
+++ src/background.js
@@ -9,9 +9,23 @@ if (typeof browser === "undefined") {
     browser = chrome;
 }
 
+const guidRegex = /[a-fA-F\d]{8}(?:-[a-fA-F\d]{4}){3}-[a-fA-F\d]{12}/g;
+const conversationsTabs = {};
+
 // Filter so the history update events are only triggered for chat.openai.com.
 const filter = {    
     url: [{hostEquals: "chat.openai.com"}]
+};
+
+// Execute content script to add download button.
+const runContentScript = async (tabId) => {    
+    try {
+        await browser.tabs.executeScript(tabId, {
+            file: "/download-button.js"
+        });
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 const listener = async (evt) => {
@@ -22,16 +36,37 @@ const listener = async (evt) => {
     
     // TODO Make logging configurable (to disable it in release mode for example) 
     console.log("history updated: " + evt.url);
+    const matches = evt.url.match(guidRegex);
+    if (matches) {
+        // Store / overwrite tab ID for use in HTTP resonse listener.
+        conversationsTabs[matches[0]] = evt.tabId;
+    }
+    
+    // Execute content script to add download button.    
+    await runContentScript(evt.tabId);
+};
+
+const responseListener = async (evt) => {
+    console.log("response from: " + evt.url);
+    
+    // Extract conversation GUID from URL.
+    const matches = evt.url.match(guidRegex);
+    if (!matches) {
+        return;
+    }
+
+    // Lookup tab by conversation GUID.
+    const tabId = conversationsTabs[matches[0]];
+    if (tabId === undefined) {
+        return;
+    }
     
     // Execute content script to add download button.
-    try {
-        await browser.tabs.executeScript(evt.tabId, {
-            file: "/download-button.js"
-        });
-    } catch (error) {
-        console.error(error);
-    }
+    await runContentScript(tabId);
 };
 
 // Listen for history changes on chat.openai.com.
 browser.webNavigation.onHistoryStateUpdated.addListener(listener, filter);
+// Listen for HTTP requests to chat.openai.com/backend-api/conversation.
+browser.webRequest.onCompleted.addListener(responseListener,
+    {urls: ["https://chat.openai.com/backend-api/conversation/*"]});
